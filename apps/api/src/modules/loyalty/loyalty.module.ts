d3bc0fdf78e67e244ea7300ac7f28f49118b6bfe
LecturerMate <ma7mood.baban@gmail.com>
2026-06-13 20:07:28 +0300

    feat(api): add public loyalty enrollment

--- apps/api/src/modules/loyalty/loyalty.module.ts
+++ apps/api/src/modules/loyalty/loyalty.module.ts
@@ -3,10 +3,12 @@ import { AuthModule } from '../auth/auth.module';
 import { BusinessesModule } from '../businesses/businesses.module';
 import { LoyaltyController } from './loyalty.controller';
 import { LoyaltyService } from './loyalty.service';
+import { PublicLoyaltyController } from './public-loyalty.controller';
+import { PublicLoyaltyService } from './public-loyalty.service';
 
 @Module({
   imports: [AuthModule, BusinessesModule],
-  controllers: [LoyaltyController],
-  providers: [LoyaltyService]
+  controllers: [LoyaltyController, PublicLoyaltyController],
+  providers: [LoyaltyService, PublicLoyaltyService]
 })
 export class LoyaltyModule {}
